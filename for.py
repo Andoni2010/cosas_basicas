a24eb56550af69df6a8bab29bf79626b5fe8c406
andoni <98291437+Andoni2010@users.noreply.github.com>
2023-07-01 10:20:06 +0200

    Add files via upload

---
+++ for.py
@@ -0,0 +1,8 @@
+lenguajes = ["PHP", "Python", "Ruby", "Java"]
+for lenguaje in lenguajes:
+    print(lenguaje)
+# Mejor el for
+i = 0
+while i < len(lenguajes):
+    print(lenguajes[i])
+    i = i + 1 
---
+++ while.py
@@ -0,0 +1,18 @@
+lenguajes = ["PHP", "Python", "Ruby", "Java"]
+
+i = 1
+while i <= 5:
+    print(i)
+    i = i + 1 # Importante para que no coja todos tus recursos e ira cambiando y sumando el valor de i
+
+i = 1
+while i <= 5:
+    print(i * "Weta. Insecto mas pesado del mundo")
+    i = i + 1 
+#no es la mejor forma es con el bucle for
+"""
+i = 0
+while i < len(lenguajes):
+    print(lenguajes[i])
+    i = i + 1 
+"""
